8324fddd5ddca9d2c4429d89350da3017adf51d5
Adi Rozenberg <104524355+adirozenberg@users.noreply.github.com>
2022-12-28 11:03:29 +0200

    Initial commit

---
+++ src/emojipedia.js
@@ -0,0 +1,46 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "👀",
+    name: "Eyes",
+    meaning: 
+    "A pair of eyes, glancing slightly to the left on most platforms. The Google version of this emoji previously showed only a single eye, despite the name of this character being eyes (plural).Sometimes used to indicate ‘pervy eyes’ to indicate approval of an attractive photo posted online; or ‘shifty eyes’ to convey a deceitful act."
+  },
+  {
+    id: 5,
+    emoji: "😵",
+    name: "Face with Crossed-Out Eyes",
+    meaning:
+    "A yellow face with X’s for eyes and a round, open mouth. Often depicted with raised or furrowed eyebrows. Facebook’s design features a purple forehead. Previously displayed with spiral eyes on some platforms.May convey a heightened or hyperbolic sense of such feelings as shock, surprise, disbelief, awe, and amazement, as if staggered to the point of disorientation (i.e., dizzy). May also represent sickness, nausea, intoxication, and death, e.g., slang I’m dead!"
+  },
+  {
+    id: 5,
+    emoji: "😒",
+    name: "Unamused Face",
+    meaning: 
+    "A yellow face with slightly raised eyebrows, a frown, and eyes looking to the side. May convey a variety of negative emotions, including irritation, displeasure, grumpiness, and skepticism, as if giving the side-eye."
+  }
+];
+
+export default emojipedia;
